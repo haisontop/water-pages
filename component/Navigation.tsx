db2af0a82d8360a8aa095799645ffd5ac1e49f2b
haisontop <haisontop@gmail.com>
2022-03-22 10:13:49 -0700

    handle contact form

--- component/Navigation.tsx
+++ component/Navigation.tsx
@@ -1,63 +1,70 @@
-import Link from 'next/link';
-import React, { useState } from 'react';
+import Link from 'next/link'
+import React, { useState } from 'react'
 
 const Navigation = () => {
-    const [open, setOpen] = useState(false)
-    return (
+  const [open, setOpen] = useState(false)
+  return (
+    <div>
+      <div className="container mx-auto hidden space-x-50.95px py-6 px-5  md:flex md:px-0 lg:max-w-container">
         <div>
-            <div className='md:flex space-x-50.95px container mx-auto lg:max-w-container py-6  px-5 md:px-0 hidden'>
-                <div>
-                    <Link href='/'>
-                        <img src="/images/Logo.png" alt="" width="147.05px" />
-                    </Link>
-                </div>
-                <div className='md:flex text-seccondery font-semibold'>
-                    <div className='mr-8 my-auto'>
-                        <Link href="/about">About</Link>
-                    </div>
-                    <div className='mr-8 my-auto'>
-                        <Link href="/solutions/OTC and Block Trading">Solutions</Link>
-                    </div>
-                    <div className='mr-8 my-auto'>
-                        <Link href="/contact">Contact</Link>
-                    </div>
-                </div>
-            </div>
-            {/* responsive navigation */}
-            <div className='py-6 mx-5 md:hidden flex justify-between'>
-                <div>
-                    <Link href='/'>
-                        <img src="/images/Logo.png" alt="" width="147.05px" />
-                    </Link>
-                </div>
-                <button onClick={() => setOpen(!open)}>
-                    <img src="images/icon/menu.png" width={40} alt="" />
-                </button>
-            </div>
-            {
-                open &&
-                <div className='bg-fill fixed h-screen w-screen text-seccondery top-0 z-50'>
-                    <div className='text-seccondery font-semibold text-center text-3xl mt-72'>
-                        <button onClick={() => setOpen(!open)} className='absolute top-5 right-5'>
-                            <img src='images/icon/cross.png' alt="" width={35} className="rotate-5" />
-                        </button>
-                        <div onClick={() => setOpen(!open)}>
-                            <Link href="/">Home</Link>
-                        </div>
-                        <div onClick={() => setOpen(!open)} className='mt-5'>
-                            <Link href="/about">About</Link>
-                        </div>
-                        <div onClick={() => setOpen(!open)} className='mt-5'>
-                            <Link href="/solutions">Solutions</Link>
-                        </div>
-                        <div onClick={() => setOpen(!open)} className='mt-5'>
-                            <Link href="/contact">Contact</Link>
-                        </div>
-                    </div>
-                </div>
-            }
+          <Link href="/">
+            <img src="/images/Logo.png" alt="" width="147.05px" />
+          </Link>
         </div>
-    );
-};
+        <div className="font-semibold text-seccondery md:flex">
+          <div className="my-auto mr-8">
+            <Link href="/about">About</Link>
+          </div>
+          <div className="my-auto mr-8">
+            <Link href="/solutions/OTC and Block Trading">Solutions</Link>
+          </div>
+          <div className="my-auto mr-8">
+            <Link href="/contact">Contact</Link>
+          </div>
+        </div>
+      </div>
+      {/* responsive navigation */}
+      <div className="mx-5 flex justify-between py-6 md:hidden">
+        <div>
+          <Link href="/">
+            <img src="/images/Logo.png" alt="" width="147.05px" />
+          </Link>
+        </div>
+        <button onClick={() => setOpen(!open)}>
+          <img src="images/icon/menu.png" width={40} alt="" />
+        </button>
+      </div>
+      {open && (
+        <div className="fixed top-0 z-50 h-screen w-screen bg-fill text-seccondery">
+          <div className="mt-72 text-center text-3xl font-semibold text-seccondery">
+            <button
+              onClick={() => setOpen(!open)}
+              className="absolute top-5 right-5"
+            >
+              <img
+                src="images/icon/cross.png"
+                alt=""
+                width={35}
+                className="rotate-5"
+              />
+            </button>
+            <div onClick={() => setOpen(!open)}>
+              <Link href="/">Home</Link>
+            </div>
+            <div onClick={() => setOpen(!open)} className="mt-5">
+              <Link href="/about">About</Link>
+            </div>
+            <div onClick={() => setOpen(!open)} className="mt-5">
+              <Link href="/solutions">Solutions</Link>
+            </div>
+            <div onClick={() => setOpen(!open)} className="mt-5">
+              <Link href="/contact">Contact</Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
-export default Navigation;
+export default Navigation
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,97 +1,271 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { Field, Formik, useField } from 'formik'
 import Layout from '../component/Layout'
 import Navigation from '../component/Navigation'
+import { COUNTRY_LIST } from '../constant/COUNTRY_LIST'
+import { PHONE_CODE } from '../constant/PHONE_CODE'
+import { useMemo } from 'react'
+
+const MySelect = ({ ...props }) => {
+  const [field, meta] = useField(props)
+  return (
+    <div>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  )
+}
 
 const Contact: NextPage = () => {
+  const handleSubmitForm = (values: any) => {
+    console.log('values', values)
+  }
+
+  const phoneCodes = useMemo(() => {
+    const codesList = Object.values(PHONE_CODE).filter((code) => !!code)
+
+    return codesList.filter(function (item, pos) {
+      return codesList.indexOf(item) == pos
+    })
+  }, [])
+
   return (
     <div>
       <Layout>
-        <div className='bg-fill pb-32 md:pb-36'>
+        <div className="bg-fill pb-32 md:pb-36">
           <Navigation />
-          <div className='container mx-auto lg:max-w-container mt-16 md:mt-91px px-5 md:px-0'>
-            <h1 className='text-4xl font-bold text-primary'>Contact</h1>
+          <div className="container mx-auto mt-16 px-5 md:mt-91px md:px-0 lg:max-w-container">
+            <h1 className="text-4xl font-bold text-primary">Contact</h1>
           </div>
         </div>
-        <div className='pt-14 md:pt-20 bg-white -mt-20 rounded-large  px-5 md:px-0'>
-          <div className='grid lg:grid-cols-2 gap-10 md:gap-20 container mx-auto lg:max-w-container'>
+        <div className="-mt-20 rounded-large bg-white px-5 pt-14  md:px-0 md:pt-20">
+          <div className="container mx-auto grid gap-10 md:gap-20 lg:max-w-container lg:grid-cols-2">
             <div>
-              <img src="/images/logo_2.png" className='w-7/12 md:w-12/12' alt="" width="" />
-              <div className='mt-7 md:mt-56.59px'>
-                <h3 className='font-bold text-2xl text-primary'>Email</h3>
-                <p className='mt-3 text-third font-light'>liquidity@waterfort.io</p>
+              <img
+                src="/images/logo_2.png"
+                className="md:w-12/12 w-7/12"
+                alt=""
+                width=""
+              />
+              <div className="mt-7 md:mt-56.59px">
+                <h3 className="text-2xl font-bold text-primary">Email</h3>
+                <p className="mt-3 font-light text-third">
+                  liquidity@waterfort.io
+                </p>
               </div>
             </div>
             <div>
-              <form>
-                <div>
-                  <label>
-                    <span className='text-seccondery'>Name</span><br />
-                    <input type="text" placeholder='Input your name' className='border-2 w-full rounded-sm outline-none px-3 mt-1 py-2' />
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Email</span><br />
-                    <input type="email" placeholder='Input your email' className='border-2 w-full rounded-sm outline-none px-3 mt-1 py-2' />
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Phone</span><br />
-                    <div className='flex border-2 rounded-sm px-2'>
-                      <select className='outline-none text-third font-semibold'>
-                        <option value="+880">880</option>
-                        <option value="+996">996</option>
-                      </select>
-                      <input type="text" placeholder='Input your phone number' className='w-full rounded-sm outline-none px-3 mt-1 py-2 ml-3' />
+              <Formik
+                initialValues={{
+                  name: '',
+                  email: '',
+                  phone: '',
+                  company: '',
+                  telegram: '',
+                  message: '',
+                  checked: [],
+                }}
+                validate={(values) => {
+                  const errors = Object.assign({})
+                  if (!values.email) {
+                    errors.email = 'Required'
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                      values.email
+                    )
+                  ) {
+                    errors.email = 'Invalid email address'
+                  }
+                  return errors
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  handleSubmitForm(values)
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                  /* and other goodies */
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <div>
+                      <label>
+                        <span className="text-seccondery">Name</span>
+                        <br />
+                        <input
+                          type="text"
+                          placeholder="Input your name"
+                          className="outline-none mt-1 w-full rounded-sm border-2 px-3 py-2"
+                          name="name"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.name}
+                        />
+                      </label>
                     </div>
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Company</span><br />
-                    <input type="text" placeholder='Input your company' className='border-2 w-full rounded-sm outline-none px-3 mt-1 py-2' />
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Telegram name</span><br />
-                    <input type="text" placeholder='Input your Telegram name' className='border-2 w-full rounded-sm outline-none px-3 mt-1 py-2' />
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Country</span><br />
-                    <select className='outline-none text-third w-full mt-1 px-3 border-2 py-2 rounded-sm'>
-                      <option>Select yout country</option>
-                      <option value="+880">Bangladesh</option>
-                      <option value="+996">China</option>
-                    </select>
-                  </label>
-                </div>
-                <div className='mt-7 text-primary'>
-                  <label>
-                    <span className='text-seccondery'>I’m Interested in</span><br />
-                    <input type="checkbox" className='mt-5' value="OTC and Block Trading" />
-                    <span className='ml-3'>OTC and Block Trading</span><br />
-                    <input type="checkbox" className='mt-3' value="Order Book Market Making" />
-                    <span className='ml-3'>Order Book Market Making</span><br />
-                    <input type="checkbox" className='mt-3' value='Algorithmic Execution Orders' />
-                    <span className='ml-3'>Algorithmic Execution Orders</span><br />
-                    <input type="checkbox" className='mt-3' value="Others" />
-                    <span className='ml-3'>Others</span><br />
-                  </label>
-                </div>
-                <div className='mt-7'>
-                  <label>
-                    <span className='text-seccondery'>Message</span><br />
-                    <textarea placeholder='Input your message' className='border-2 h-40 w-full rounded-sm outline-none px-3 mt-1 py-2' />
-                  </label>
-                </div>
-                <button className='button text-white px-7 py-3 mt-7 rounded-md'>submit</button>
-              </form>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Email</span>
+                        <br />
+                        <input
+                          type="email"
+                          placeholder="Input your email"
+                          className="outline-none mt-1 w-full rounded-sm border-2 px-3 py-2"
+                          name="email"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                        />
+                      </label>
+                    </div>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Phone</span>
+                        <br />
+                        <div className="flex rounded-sm border-2 px-2">
+                          <MySelect
+                            className="outline-none h-full font-semibold text-third"
+                            name="phoneCode"
+                          >
+                            {phoneCodes.map((code) => (
+                              <option key={code} value={code}>
+                                {code}
+                              </option>
+                            ))}
+                          </MySelect>
+                          <input
+                            type="text"
+                            placeholder="Input your phone number"
+                            className="outline-none mt-1 ml-3 w-full rounded-sm px-3 py-2"
+                            name="phone"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.phone}
+                          />
+                        </div>
+                      </label>
+                    </div>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Company</span>
+                        <br />
+                        <input
+                          type="text"
+                          placeholder="Input your company"
+                          className="outline-none mt-1 w-full rounded-sm border-2 px-3 py-2"
+                          name="company"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.company}
+                        />
+                      </label>
+                    </div>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Telegram name</span>
+                        <br />
+                        <input
+                          type="text"
+                          placeholder="Input your Telegram name"
+                          className="outline-none mt-1 w-full rounded-sm border-2 px-3 py-2"
+                          name="telegram"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.telegram}
+                        />
+                      </label>
+                    </div>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Country</span>
+                        <br />
+                        <MySelect
+                          className="outline-none mt-1 w-full rounded-sm border-2 px-3 py-2 text-third"
+                          name="country"
+                        >
+                          <option>Select yout country</option>
+                          {COUNTRY_LIST.map((country) => (
+                            <option key={country} value={country}>
+                              {country}
+                            </option>
+                          ))}
+                        </MySelect>
+                      </label>
+                    </div>
+                    <div className="mt-7 text-primary">
+                      <label>
+                        <span className="text-seccondery">
+                          I’m Interested in
+                        </span>
+                        <br />
+                        <div role="group" aria-labelledby="checkbox-group">
+                          <Field
+                            type="checkbox"
+                            name="interested"
+                            value="OTC and Block Trading"
+                            className="mt-5"
+                          />
+                          <span className="ml-3">OTC and Block Trading</span>
+                          <br />
+                          <Field
+                            type="checkbox"
+                            name="interested"
+                            value="Order Book Market Making"
+                            className="mt-3"
+                          />
+                          <span className="ml-3">Order Book Market Making</span>
+                          <br />
+                          <Field
+                            type="checkbox"
+                            name="interested"
+                            value="Algorithmic Execution Orders"
+                            className="mt-3"
+                          />
+                          <span className="ml-3">
+                            Algorithmic Execution Orders
+                          </span>
+                          <br />
+                          <Field
+                            type="checkbox"
+                            name="interested"
+                            value="Others"
+                            className="mt-3"
+                          />
+                          <span className="ml-3">Others</span>
+                        </div>
+                        <br />
+                      </label>
+                    </div>
+                    <div className="mt-7">
+                      <label>
+                        <span className="text-seccondery">Message</span>
+                        <br />
+                        <textarea
+                          placeholder="Input your message"
+                          className="outline-none mt-1 h-40 w-full rounded-sm border-2 px-3 py-2"
+                          name="message"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.message}
+                        />
+                      </label>
+                    </div>
+                    <button
+                      className="button mt-7 rounded-md px-7 py-3 text-white"
+                      type="submit"
+                    >
+                      submit
+                    </button>
+                  </form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>
@@ -101,4 +275,3 @@ const Contact: NextPage = () => {
 }
 
 export default Contact
-
